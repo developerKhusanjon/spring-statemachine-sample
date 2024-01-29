9377ae7fca1e5888458ddc8d14ff954555d8d8af
Khusanjon <developerkhusanjon@gmail.com>
2024-01-30 01:06:14 +0500

    Service layer added

---
+++ src/main/java/com/khusanjon/springstatemachinesample/service/impl/OfferingServiceImpl.java
@@ -0,0 +1,13 @@
+package com.khusanjon.springstatemachinesample.service.impl;
+
+import com.khusanjon.springstatemachinesample.model.OfferDto;
+import com.khusanjon.springstatemachinesample.service.OfferingService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OfferingServiceImpl implements OfferingService {
+    @Override
+    public OfferDto doOffer(OfferDto offerDto) {
+        return null;
+    }
+}
